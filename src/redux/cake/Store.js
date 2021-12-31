3cbc0c862155633164973941477c6d1a541edbb3
chandrakanth2103 <chandrakanth.sixthblock@gmail.com>
2021-12-31 13:25:58 +0530

    removed te src folder

--- src/redux/cake/Store.js
+++
@@ -1,8 +0,0 @@
-import {createStore,applyMiddleware} from 'redux'
-import cakeReducer from './CakeReducer'
-import rootReducer from '../rootReducer'
-import {logger} from 'redux-logger';
-
-const Store= createStore(rootReducer,applyMiddleware(logger))
-
-export default Store
